16a4c7691ab3bf814d95b7f6f29e775aa1dd671b
Safia25 <safia.benaouda@gmail.com>
2016-04-12 14:39:21 -0700

    Added files via upload
    Updated resumehelper

--- resumeBuilder.js
+++ resumeBuilder.js
@@ -1,7 +1,72 @@
-var email = "safia@gmail.com";
+var bio = { 
+	"name": "Safia",
+	"role": "Designer",
+	"contact": {
+				"email": "Safia.Benaouda@gmail.com",
+				"location": "Venice Beach",
+				"phone": "925-785-3183"
+				},
+	"welcomemessage": "Prepare to be impressed.",
+	"skills": ["awesomeness", "graphic design", "photoshop", "InDesign"],
+	"image": "images/safia.jpg"
+}
 
-var newEmail =
-	email.replace("gmail", "directv");
+var education = {
+	"schools": [
+		{
+		"name": "UC Santa Barbara",
+		"degree": "BA",
+		"major": "Communication"
+		},
+		{
+		"name": "University Goldsmiths London",
+		"degree": "Masters",
+		"major": "Digital Media"
+		}
+	]
+}
 
-console.log(email);
-console.log(newEmail);	
+var work = {
+	"jobs": 
+		{
+		"employer": "Directv",
+		"title": "Specialist",
+		"location": "El Segundo",
+		"dates": "2013 - 2016",
+		"description": "Design Specialist and trainer."
+		}
+}
+
+var projects = {
+"projects": [
+		{
+		"title": "Portfolio",
+		"dates": "2016",
+		"description": "This killer online portfolio."
+		},
+		{
+		"title": "Portfolio II",
+		"dates": "2017",
+		"description": "This is my second killer online portfolio."
+		}
+	]
+}
+
+bio.display = function() {
+var formattedName = HTMLheaderName.replace("%data%",bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%",bio.role);
+var formattedimage = HTMLbioPic.replace("%data%",bio.image);
+var formattedemail = HTMLemail.replace("%data%",bio.contact.email);
+var formattedlocation = HTMLlocation.replace("%data%",bio.contact.location);
+}
+
+$("#header").prepend(formattedRole).prepend(formattedName);
+
+education.display = function () {
+var formattedschoolName = HTMLschoolName.replace("%data%",education.schools.name);
+var formattedschoolDegree = HTMLschoolDegree.replace("%data%",education.schools.degree);
+var formattedschoolDates = HTMLschoolDates.replace("%data%",education.schools.dates);
+var formattedschoolLocation = HTMLschoolLocation.replace("%data%",education.schools.location);
+
+$("#education").append(formattedschoolName).append(formattedschoolLocation).append(formattedschoolDates).append(formattedschoolDegree);
+}
